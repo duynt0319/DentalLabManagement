42cb365f19e09a251b3640e2c35eddfe8d9eb3d2
chinhhoang <chinhnlhse140506@fpt.edu.vn>
2023-10-18 21:32:27 +0700

    add model

---
+++ DentalLabManagement.DataTier/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DentalLabManagement.DataTier.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            GroupStages = new HashSet<GroupStage>();
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public virtual ICollection<GroupStage> GroupStages { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
